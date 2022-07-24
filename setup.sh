9192614b1712abec014b6cf51f0c02d0793c2814
Ritik <csritik@gmail.com>
2022-07-24 22:03:31 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"rohith@onlinedegree.iitm.ac.in\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
