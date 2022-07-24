ca37e36d2edaf15ddfad3ca382d02d0751a1eea0
Ritik <csritik@gmail.com>
2022-07-24 22:28:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Division", page_icon=":heavy_division_sign:",layo
 st.subheader("Division :heavy_division_sign: App")
 st.title("Divide 2 Numbers here : ")
 st.write("A web Division App Made using Python :")
-st.write("[Links](https://www.google.com)")
+#st.write("[Links](https://www.google.com)")
 
 st.write('*Expression : a/b*')
 a = st.number_input('Enter a :')
